534f4762b39141f9169adeb58ed3fda51d25fd9f
Farooq-cloud <83739025+Farooq-cloud@users.noreply.github.com>
2023-10-29 22:03:56 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -10,17 +10,14 @@ instance_type = t2.medium
 security_group = Terraform-sgroup-oct-2023
 tag_name = my-Terraform-Instance
 ami_id = ami-0f5ee92e2d63afc18
+acl = private
+bucket_prefix = my-statefile-backup
+versioning = true
 dynamodb_table_name = my-dynamo-db-table-tf
 dynamodb_billing_mode = PAY_PER_REQUEST
 dynamodb_hash_key = my-hash-key
 dynamodb_range_key = my-range-key
 dynamodb_attributes = dynamodb_attributes
-}
-
-module "s3_bucket" {
-source = "./modules/ec2_instance"
-bucket_prefix = my-statefile-backup
-versioning = true
 }
 
 

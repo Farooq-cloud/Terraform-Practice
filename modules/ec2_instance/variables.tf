71d3a8b8a1ba0a90367e39d1c58e91c16753beb6
Farooq-cloud <83739025+Farooq-cloud@users.noreply.github.com>
2023-10-30 16:06:20 +0530

    Update variables.tf

--- modules/ec2_instance/variables.tf
+++ modules/ec2_instance/variables.tf
@@ -1,88 +1,71 @@
 variable "aws_region" {
-       description = "The AWS region to create things in." 
-       default     = "ap-south-1" 
+    description = "The AWS region to create things in."
 }
 
-variable "key_name" { 
-    description = " SSH keys to connect to ec2 instance" 
-    default     =  "DevOps-Practice" 
+variable "key_name" {
+    description = "SSH keys to connect to the EC2 instance"
 }
 
-variable "instance_type" { 
-    description = "instance type for ec2" 
-    default     =  "t2.micro" 
+variable "instance_type" {
+    description = "Instance type for EC2"
 }
 
-variable "security_group" { 
-    description = "Name of security group" 
-    default     = "Terraform-sgroup-oct-2023" 
+variable "security_group" {
+    description = "Name of security group"
 }
 
-variable "tag_name" { 
-    description = "Tag Name of for Ec2 instance" 
-    default     = "my-Terraform-Instance" 
-} 
-variable "ami_id" { 
-    description = "AMI for Ubuntu Ec2 instance" 
-    default     = "ami-0f5ee92e2d63afc18" 
+variable "tag_name" {
+    description = "Tag Name for EC2 instance"
 }
+
+variable "ami_id" {
+    description = "AMI for Ubuntu EC2 instance"
+}
+
 variable "versioning" {
     type        = bool
     description = "(Optional) A state of versioning."
-    default     = true
 }
+
 variable "acl" {
     type        = string
-    description = " Defaults to private "
-    default     = "private"
+    description = "Defaults to private"
 }
+
 variable "bucket_prefix" {
     type        = string
     description = "(required since we are not using 'bucket') Creates a unique bucket name beginning with the specified prefix"
-    default     = "my-statefile-backup"
 }
+
 variable "dynamodb_table_name" {
-description = "The name of the DynamoDB table."
-type        = string
-default     = "my-dynamo-db-table-tf"
+    description = "The name of the DynamoDB table."
+    type        = string
 }
+
 variable "dynamodb_billing_mode" {
-  description = "The billing mode for the DynamoDB table (e.g., PAY_PER_REQUEST or PROVISIONED)."
-  type        = string
-  default     = "PAY_PER_REQUEST"
+    description = "The billing mode for the DynamoDB table (e.g., PAY_PER_REQUEST or PROVISIONED)."
+    type        = string
 }
+
 variable "dynamodb_hash_key" {
-  description = "The hash key for the DynamoDB table."
-  type        = string
-  default     = "my-hash-key"
+    description = "The hash key for the DynamoDB table."
+    type        = string
 }
+
 variable "dynamodb_range_key" {
-  description = "The range key for the DynamoDB table."
-  type        = string
-  default     = "my-range-key"
+    description = "The range key for the DynamoDB table."
+    type        = string
 }
+
 variable "dynamodb_attributes" {
-  description = "A list of attribute configurations for the DynamoDB table."
-  type        = list(object({
-    name = string
-    type = string
-  }))
-  default = [
-    {
-      name = "my-hash-key"
-      type = "S"
-    },
-    {
-      name = "my-range-key"
-      type = "N"
-    }
-  ]
+    description = "A list of attribute configurations for the DynamoDB table."
+    type        = list(object({
+        name = string
+        type = string
+    }))
 }
+
 variable "tags" {
     type        = map
     description = "(Optional) A mapping of tags to assign to the bucket."
-    default     = {
-        environment = "DEV"
-        terraform   = "true"
-    }
 }

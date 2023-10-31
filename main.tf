9631779eb98edc837f1d62f805013033dc475448
Farooq-cloud <83739025+Farooq-cloud@users.noreply.github.com>
2023-10-31 20:04:20 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,20 +3,16 @@ provider "aws" {
 }
 module "ec2_instance" {
 source = "./modules/ec2_instance"
-aws_region = ap-south-1
-key_name = DevOps-Practice
-instance_type = t2.medium
-security_group = Terraform-sgroup-oct-2023
-tag_name = my-Terraform-Instance
-tags = var.common_tags
-ami_id = ami-0f5ee92e2d63afc18
-acl = private
-bucket_prefix = my-statefile-backup
-versioning = true
-dynamodb_table_name = my-dynamo-db-table-tf
-dynamodb_billing_mode = PAY_PER_REQUEST
-dynamodb_hash_key = my-hash-key
-dynamodb_range_key = my-range-key
-dynamodb_attributes = dynamodb_attributes
 }
 
+module "aws_dynamodb_table" {
+source = "./modules/ec2_instance"
+}
+
+module "aws_dynamodb_table" {
+source = "./modules/ec2_instance"
+}
+
+module "s3_bucket" {
+source = "./modules/ec2_instance"
+}

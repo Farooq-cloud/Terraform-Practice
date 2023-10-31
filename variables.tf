085de544bc1515ac1da5efac6f6bbbb6fd9d35c1
Farooq-cloud <83739025+Farooq-cloud@users.noreply.github.com>
2023-10-31 18:09:52 +0530

    Delete variables.tf

--- variables.tf
+++
@@ -1,8 +0,0 @@
-variable "common_tags" {
-  description = "Common tags to be applied to resources"
-  type        = map(string)
-  default     = {
-    Environment = "DEV",
-    terraform       = "true"
-  }
-}

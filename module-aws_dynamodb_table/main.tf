c1a364caf1ef744ee64cde3519eb30377af0b142
Farooq-cloud <83739025+Farooq-cloud@users.noreply.github.com>
2023-10-31 18:54:20 +0530

    Create main.tf

---
+++ module-aws_dynamodb_table/main.tf
@@ -0,0 +1,16 @@
+resource "aws_dynamodb_table" "my-dynamo-db-table-tf" {
+  name           = "my-dynamo-db-table-tf"
+  billing_mode   = "PAY_PER_REQUEST"
+  hash_key       = "my-hash-key"
+  range_key      = "my-range-key"
+
+  attribute {
+    name = "my-hash-key"
+    type = "S"
+  }
+attribute {
+    name = "my-range-key"
+    type = "N"
+  }
+
+}

44fc519104338741cb285d64f25f9b247b41b23c
Farooq-cloud <83739025+Farooq-cloud@users.noreply.github.com>
2023-10-31 20:16:45 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,13 +2,13 @@ provider "aws" {
     region = "ap-south1"
 }
 module "ec2_instance" {
-source = "./modules/ec2_instance"
+source = "./module/ec2_instance"
 }
 
 module "aws_dynamodb_table" {
-source = "./modules/ec2_instance"
+source = "./module/aws_dynamodb_table"
 }
 
 module "s3_bucket" {
-source = "./modules/ec2_instance"
+source = "./module/s3_bucket"
 }

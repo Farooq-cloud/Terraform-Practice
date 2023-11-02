4d14b2ab06400d22901fe3490cd40920caaa931e
Farooq-cloud <83739025+Farooq-cloud@users.noreply.github.com>
2023-11-03 00:58:21 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -41,6 +41,41 @@ variable "bucket_prefix" {
     description = "(required since we are not using 'bucket') Creates a unique bucket name beginning with the specified prefix"
     default     = "my-statefile-backup"
 }
+
+ariable "table_name" {
+  description = "The name of the DynamoDB table."
+  default     = "my-dynamo-db-table-tf"
+}
+
+variable "billing_mode" {
+  description = "The billing mode for the DynamoDB table (PROVISIONED or PAY_PER_REQUEST)."
+  default     = "PAY_PER_REQUEST"
+}
+
+variable "hash_key" {
+  description = "The hash key for the DynamoDB table."
+  default     = "my-hash-key"
+}
+
+variable "range_key" {
+  description = "The range key for the DynamoDB table."
+  default     = "my-range-key"
+}
+
+variable "attributes" {
+  description = "A list of attribute definitions for the DynamoDB table."
+  type        = list(map(string))
+  default     = [
+    {
+      name = "my-hash-key",
+      type = "S"
+    },
+    {
+      name = "my-range-key",
+      type = "N"
+    }
+  ]
+}
 variable "tags" {
     type        = map
     description = "(Optional) A mapping of tags to assign to the bucket."

62d9a0487a91f4335fcae4cf92798fe987f50932
Farooq-cloud <83739025+Farooq-cloud@users.noreply.github.com>
2023-11-07 14:50:12 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -45,7 +45,6 @@ resource "aws_security_group" "Terraform-sg" {
 }
 
 resource "aws_instance" "my-Terraform-Instance" {
-name = "my-Terraform-Instance"
 ami           = "ami-0287a05f0ef0e9d9a"
 key_name = "DevOps-Practice"
 instance_type = "t2.medium"

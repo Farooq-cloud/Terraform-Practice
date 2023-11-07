877df0ce19e6f1c876a3124abe2bbaaab551aba6
Farooq-cloud <83739025+Farooq-cloud@users.noreply.github.com>
2023-11-07 13:07:56 +0530

    Update main.tf

--- module-s3_bucket/main.tf
+++ module-s3_bucket/main.tf
@@ -1,10 +1,7 @@
+provider "aws" {
+  region = "ap-south-1"
+  # Add your AWS access and secret keys here, or use an AWS profile
+}
+
 resource "aws_s3_bucket" "my-s3-bucket" {
-  bucket_prefix = var.bucket_prefix
-  acl = var.acl
-  
-   versioning {
-    enabled = var.versioning
-  }
-  
-  tags = var.tags
 }

c0df9760d2603122755aa138fb927885dde97138
Farooq-cloud <83739025+Farooq-cloud@users.noreply.github.com>
2023-11-07 14:07:24 +0530

    Update main.tf

--- Dev/main.tf
+++ Dev/main.tf
@@ -5,17 +5,17 @@ provider "aws" {
 # module for creating ec2_instance
 
 module "ec2_instance" {
-source = ".//module-ec2_instance"
+source = "./module-ec2_instance"
 }
 
 # Module Creating dynamodb table
 
 module "aws_dynamodb_table" {
-source = ".//module-aws_dynamodb_table"
+source = "./module-aws_dynamodb_table"
 }
 
 # module creating s3 bucket
 
 module "s3_bucket" {
-source = ".//module-s3_bucket"
+source = "./module-s3_bucket"
 }

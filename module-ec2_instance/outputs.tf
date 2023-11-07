ba2c41d0e9b821fad3162299b62021f12117c814
Farooq-cloud <83739025+Farooq-cloud@users.noreply.github.com>
2023-11-07 13:16:29 +0530

    Update outputs.tf

--- module-ec2_instance/outputs.tf
+++ module-ec2_instance/outputs.tf
@@ -2,5 +2,8 @@ output "instance_id" {
   value = aws_instance.my-terraform-insatnce.id
 }
 output "public_ip" {
-  value = aws_instance.my-terraform-insatnce.id
+  value = aws_instance.my-terraform-insatnce.public_ip
+}
+output "private-ip-address" {
+  value =aws_instance.my-terraform-insatnce.private-ip
 }

696df6b79bf97acd45bceb00ad2c89efeeb812c1
Farooq-cloud <83739025+Farooq-cloud@users.noreply.github.com>
2023-11-07 13:17:30 +0530

    Update variables.tf

--- module-ec2_instance/variables.tf
+++ module-ec2_instance/variables.tf
@@ -2,10 +2,6 @@ variable "aws_region" {
   description = "The AWS region where the resources should be created."
 }
 
-variable "security_group" {
-  description = "The name for the security group."
-}
-
 variable "ami_id" {
   description = "The ID of the AMI to use for the EC2 instance."
 }
@@ -18,6 +14,3 @@ variable "instance_type" {
   description = "The instance type for the EC2 instance."
 }
 
-variable "tag_name" {
-  description = "The Name tag for the EC2 instance."
-}
